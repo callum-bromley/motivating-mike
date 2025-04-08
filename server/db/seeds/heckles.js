70e27574b9257c5b616619de9839fba08975b866
Bryn van Vliet <bryn.music@gmail.com>
2025-04-09 09:54:30 +1200

    add celebrations

--- server/db/seeds/heckles.js
+++ server/db/seeds/heckles.js
@@ -9,6 +9,7 @@ export async function seed(knex) {
       todo_id: 1,
       avatar_id: 1,
       heckles: 'Come on, you can do it!',
+      celebrations: 'Nice!',
       severity: 3,
     },
     {
@@ -16,6 +17,7 @@ export async function seed(knex) {
       todo_id: 2,
       avatar_id: 1,
       heckles: 'Put your back into it!',
+      celebrations: 'You done did it',
       severity: 2,
     },
     {
@@ -23,13 +25,15 @@ export async function seed(knex) {
       todo_id: 3,
       avatar_id: 1,
       heckles: 'Dude, HURRY UP. I got a golf match to get to',
+      celebrations: 'Bloody good job',
       severity: 1,
     },
     {
       id: 4,
       todo_id: 4,
       avatar_id: 2,
-      heckles: 'Complete this in your lunch break',
+      heckles: 'Come on lets get started',
+      celebrations: 'Excellent',
       severity: 3,
     },
     {
@@ -37,6 +41,7 @@ export async function seed(knex) {
       todo_id: 5,
       avatar_id: 2,
       heckles: "You're headed for detention!",
+      celebrations: 'Great work!',
       severity: 2,
     },
     {
@@ -44,6 +49,7 @@ export async function seed(knex) {
       todo_id: 6,
       avatar_id: 2,
       heckles: "I'm not paid enough for this",
+      celebrations: 'Phew, You made it!',
       severity: 1,
     },
     {
@@ -51,6 +57,7 @@ export async function seed(knex) {
       todo_id: 7,
       avatar_id: 3,
       heckles: 'Slip slop slap and sthmash out your goals!',
+      celebrations: 'woop woop',
       severity: 3,
     },
     {
@@ -58,6 +65,7 @@ export async function seed(knex) {
       todo_id: 8,
       avatar_id: 3,
       heckles: 'Whoopsty doopsty you should probably start that',
+      celebrations: 'Holy tamoli!',
       severity: 2,
     },
     {
@@ -65,6 +73,7 @@ export async function seed(knex) {
       todo_id: 9,
       avatar_id: 3,
       heckles: "Scadoosh! You're in trouble. Glop glop glop!",
+      celebrations: 'Wow im so proud of you! Sklip sklamp plomp!',
       severity: 1,
     },
   ])
