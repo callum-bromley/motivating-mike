99de057dcd89a74399779be07ae378a6287c02f7
Phill Keelan <philliprpkeelan@gmail.com>
2025-04-09 10:57:54 +1200

    Updated seed file 02_todos.js

--- server/db/seeds/02_todos.js
+++ server/db/seeds/02_todos.js
@@ -7,7 +7,8 @@ export async function seed(knex) {
       urgency: 3,
       created: '2025-04-25 08:30:56',
       due: '2025-04-25 18:30:56',
-      user_id: 2
+      user_id: 2,
+      avatar_id: 3
     },
     {
       id: 1,
@@ -16,7 +17,8 @@ export async function seed(knex) {
       urgency: 1,
       created: '2025-04-25 08:30:56',
       due: '2025-04-25 10:30:56',
-      user_id: 1
+      user_id: 1,
+      avatar_id: 3
     },
   ]);
 }
