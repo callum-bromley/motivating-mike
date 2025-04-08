abf688de4d3cd6d744000c65818f7a247ede7f77
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-08 21:46:41 +1200

    add basic file structure for server routes

---
+++ server/routes/todoStatus.ts
@@ -0,0 +1,5 @@
+// GET /api/v1/todos/complete
+//
+// PATCH /api/v1/todo/id/status
+//
+// PATCH /api/v1/todo/id/urgency
---
+++ server/routes/todos.ts
@@ -0,0 +1,13 @@
+// GET /api/v1/todos
+//
+// GET /api/v1/todos/id
+//
+// GET /api/v1/todos/urgency
+//
+// GET /api/v1/todos/status
+//
+// POST /api/v1/todo
+//
+// PATCH /api/v1/todo/id
+//
+// DELETE /api/v1/todo/id
