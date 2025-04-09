3c8681adbb2296a8e50363c3be8bf42bcdade13d
Bryn van Vliet <bryn.music@gmail.com>
2025-04-10 10:43:28 +1200

    add use-add-todo function

--- client/apis/use-add-todo.ts
+++ client/apis/use-add-todo.ts
@@ -1,1 +1,9 @@
-placeholder
+import request from "superagent";
+import { TodoData, Todo } from "../Models/todos";
+
+export async function addTodo(newTodo: TodoData) {
+  const response = await request.post('/api/v1/todos').send(newTodo)
+  console.log(response.body)
+  return response.body as Todo
+}
+
