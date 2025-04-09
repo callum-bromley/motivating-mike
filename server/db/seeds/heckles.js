ff2a7fb2414e53d9b60f55b43a5e9cdee795e6fb
Joel-Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-09 21:59:28 +1200

    fix primary key duplicates between heckles and celebration files

--- server/db/seeds/heckles.js
+++ server/db/seeds/heckles.js
@@ -5,80 +5,59 @@
 export async function seed(knex) {
   await knex('heckles').insert([
     {
-      id: 1,
-      heckle: 'Come on, you can do it!',
-
-            avatar_id: 1,
-
-      severity: 3,
+      id: 10,
+      heckle: 'Come on, you can do it!',
+      severity: 2,
+      avatar_id: 1
     },
     {
-      heckle: 'Put your back into it!',
+      id: 11,
       heckle: 'Put your back into it!',
-            avatar_id: 1,
-
-
-      severity: 2,
+      severity: 1,
+      avatar_id: 1
     },
-      heckle: 'Dude, HURRY UP. I got a golf match to get to',
-      avatar_id: 1,
-            avatar_id: 1,
-
+    {
+      id: 12,
       heckle: 'Dude, HURRY UP. I got a golf match to get to',
-
-      severity: 1,
-      heckle: 'Come on lets get started',
-      todo_id: 4,
-            avatar_id: 2,
-
-      avatar_id: 2,
-      heckle: 'Come on lets get started',
-
-      heckle: "You're headed for detention!",
-      id: 5,
-            avatar_id: 2,
-
-      todo_id: 5,
-      avatar_id: 2,
-      heckle: "You're headed for detention!",
-      heckle: "I'm not paid enough for this",
-    {
-            avatar_id: 2,
-
-      id: 6,
-      todo_id: 6,
-      avatar_id: 2,
-      heckle: 'Slip slop slap and sthmash out your goals!',
-    },
-            avatar_id: 3,
-
-    {
-      id: 7,
-      todo_id: 7,
-      heckle: 'Whoopsty doopsty you should probably start that',
       severity: 3,
-            avatar_id: 3,
-
+      avatar_id: 1
     },
     {
-      id: 8,
-      heckle: "Scadoosh! You're in trouble. Glop glop glop!",
-
-            avatar_id: 3,
-
+      id: 13,
+      heckle: 'come on you can do it',
       severity: 2,
+      avatar_id: 3
     },
     {
-      id: 9,
-      todo_id: 9,
-      avatar_id: 3,
-      heckle: "Scadoosh! You're in trouble. Glop glop glop!",
-
+      id: 14,
+      heckle: 'Come on lets get started',
       severity: 1,
+      avatar_id: 2
     },
+    {
+      id: 15,
+      heckle: "You're headed for detention!",
+      severity: 2,
+      avatar_id: 2
+    },
+    {
+      id: 16,
+      heckle: "I'm not paid enough for this",
+      severity: 3,
+      avatar_id: 3
+
+    },
+    {
+      id: 17,
+      heckle: 'Slip slop slap and sthmash out your goals!',
+      severity: 1,
+      avatar_id: 3
+    },
+    {
+      id: 18,
+      heckle: 'Whoopsty doopsty you should probably start that',
+      severity: 3,
+      avatar_id: 3
+    }
   ])
 }
-
-// avatar_id 1 = Mike
-// avatar_id 2 = Mandy
-// avatar_id 3 = Slappy
