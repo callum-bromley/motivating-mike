8e6d4d359843693cb6cd1a96b4d02f22d6408290
Phill Keelan <philliprpkeelan@gmail.com>
2025-04-09 12:09:36 +1200

    Updated seed file todos.js

--- server/db/seeds/02_todos.js
+++ server/db/seeds/02_todos.js
@@ -3,22 +3,29 @@ export async function seed(knex) {
     {
       id: 1,
       task: 'Cook',
-      status: 0,
+      completed: null,
       urgency: 3,
-      created: '2025-04-25 08:30:56',
-      due: '2025-04-25 18:30:56',
+      created: '2025-04-25 08:30:58',
+      due: '2025-04-25 18:30:26',
       user_id: 2,
-      avatar_id: 3
     },
     {
-      id: 1,
-      task: 'Cook',
-      status: 0,
+      id: 2,
+      task: 'Clean',
+      completed: '2025-04-25 9:30:53',
       urgency: 1,
       created: '2025-04-25 08:30:56',
+      due: '2025-04-25 10:30:43',
+      user_id: 1,
+    },
+    {
+      id: 2,
+      task: 'Shower',
+      completed: null,
+      urgency: 1,
+      created: '2025-04-25 08:30:10',
       due: '2025-04-25 10:30:56',
       user_id: 1,
-      avatar_id: 3
     },
-  ]);
+  ])
 }
