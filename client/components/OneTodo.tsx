7e823b2e3bae42da61248c20f8f267f6f58f4532
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-09 13:06:49 +1200

    get OntTodo component showing on HomePage

--- client/components/OneTodo.tsx
+++ client/components/OneTodo.tsx
@@ -1,1 +1,8 @@
-Placeholder
+function OneTodo() {
+  return (
+    <h1>One todo component placeholder</h1>
+  )
+}
+
+export default OneTodo
+
--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,6 +1,11 @@
+import OneTodo from "../components/OneTodo";
+
 export default function Home() {
   return (
-    <h1>Home Page Component placeholder</h1>
+    <>
+      <h1>Home Page Component placeholder</h1>
+      <OneTodo />
+    </>
   )
 }
 
