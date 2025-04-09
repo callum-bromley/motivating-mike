e13d2cfe9d25c1c41681b91dd55273c46aa4a440
Bryn van Vliet <bryn.music@gmail.com>
2025-04-10 10:44:58 +1200

    add use-delete-todo function

--- client/apis/use-delete-todo.ts
+++ client/apis/use-delete-todo.ts
@@ -1,1 +1,7 @@
-Placeholder
+import request from 'superagent'
+// import { TodoData, Todo } from "../Models/todos";
+
+export async function deleteTodo(id: number) {
+  const response = await request.delete(`/api/v1/songs/${id}`)
+  return response
+}
