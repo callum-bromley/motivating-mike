cae79e5db20370a79f4ce5b8f978a4fb4371c650
Bryn van Vliet <bryn.music@gmail.com>
2025-04-10 09:37:23 +1200

    add addTodo

--- server/db/functions/todos.ts
+++ server/db/functions/todos.ts
@@ -1,3 +1,7 @@
+import { TodoData } from "../../../client/Models/todos"
+import connection from "../connection"
+// import connection from './connection'
+
 //This where the functions for
 
 // getTodos
@@ -5,3 +9,26 @@
 // editTodo
 // deleteTodo
 // AddTodo
+
+
+// add todo
+export async function addTodo(data: TodoData) {
+  const { task, urgency, created, due, completed, userId} = data
+  const newTodo = {
+    // id,
+    task,
+    urgency,
+    created,
+    due,
+    completed,
+    user_id: userId,
+  }
+  const results = await connection('todos').insert(newTodo)
+  return results
+}
+
+// edit todo 
+
+
+
+// delete todo
