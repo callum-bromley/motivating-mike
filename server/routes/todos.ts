306373c681522781459adc1b7d1dc5d857b466cf
Bryn van Vliet <bryn.music@gmail.com>
2025-04-10 09:54:59 +1200

    complete routes for add, update, delete todos

--- server/routes/todos.ts
+++ server/routes/todos.ts
@@ -1,3 +1,7 @@
+import express from 'express'
+import * as todo from '../db/functions/todos.ts'
+const router = express.Router()
+
 // GET /api/v1/todos
 //
 // GET /api/v1/todos/id
@@ -7,7 +11,47 @@
 // GET /api/v1/todos/status
 //
 // POST /api/v1/todo
+
+router.post('/', async (req, res, next) => {
+  try {
+    await todo.addTodo(req.body)
+    res.sendStatus(204)
+  } catch (e) {
+    next(e)
+  }
+})
+
 //
 // PATCH /api/v1/todo/id
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const { task, urgency, created, due, completed, userId } = req.body
+    const id = Number(req.params.id)
+
+    await todo.updateTodo({
+      id,
+      task,
+      urgency,
+      created,
+      due,
+      completed,
+      userId,
+    })
+    res.sendStatus(204)
+  } catch (e) {
+    next(e)
+  }
+})
 //
 // DELETE /api/v1/todo/id
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const id = Number(req.params.id)
+    await todo.deleteTodo(id)
+    res.sendStatus(204)
+  } catch (e) {
+    next(e)
+  }
+})
