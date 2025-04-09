49f42edf4f99a52ad274e44af5426297a86821ad
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-10 09:45:14 +1200

    add useUserData code from Phill

--- client/apis/use-user-data.ts
+++ client/apis/use-user-data.ts
@@ -1,2 +1,16 @@
-Placeholder
+import request from 'superagent'
+import { useQuery } from '@tanstack/react-query'
 
+import { User } from '../models/users.ts'
+
+export default function useUserData(id: number) {
+  const query = useQuery({
+    queryKey: ['users', id],
+    queryFn: async () => {
+      const res = await request.get(`/api/v1/users/${id}`)
+      return res.body as User
+    },
+  })
+
+  return { ...query }
+}
