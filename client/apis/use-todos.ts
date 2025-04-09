00738637a5545af6ac4d20a361dad0d81e5f1043
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-10 09:43:58 +1200

    update Models to models, and update useTodos

--- client/apis/use-todos.ts
+++ client/apis/use-todos.ts
@@ -1,16 +1,19 @@
-import request from "superagent";
-import { Todo } from "../Models/todos";
-import { useQuery } from "@tanstack/react-query";
+import request from 'superagent'
+import { useQuery } from '@tanstack/react-query'
+
+import { Todo } from '../models/todos.ts'
 
 export default function useTodos() {
   return useQuery({
     queryKey: ['todos'],
     queryFn: async () => {
-      const res = await request.get('api/v1/todos')
+      const res = await request.get('/api/v1/todos')
       if (res.ok) {
+        console.log(res.body)
         return res.body as Todo[]
       }
+
       throw new Error(res.text)
-    }
+    },
   })
 }
