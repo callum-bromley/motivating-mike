fb3274a398d9efb883c9ba6f9e48e0a178684065
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-09 14:18:03 +1200

    start setting up map

--- client/components/OneTodo.tsx
+++ client/components/OneTodo.tsx
@@ -1,31 +1,87 @@
-import useTodos from "../apis/use-todos"
+// import useTodos from "../apis/use-todos"
+
+// interface Props {
+//   userId: number
+// }
+const userId = 1
+
+const todos = [{
+  id: '1',
+  task: 'wash cat',
+  urgency: 3,
+  created: '2025',
+  due: '2026',
+  completed: '',
+  userId: 1,
+},
+{
+  id: '2',
+  task: 'Tip Cow',
+  urgency: 3,
+  created: '2025',
+  due: '2026',
+  completed: '',
+  userId: 1,
+},
+{
+  id: '3',
+  task: 'Shear Sheep',
+  urgency: 1,
+  created: '2025',
+  due: '2026',
+  completed: '',
+  userId: 1,
+},
+{
+  id: '4',
+  task: 'Make up excuses',
+  urgency: 3,
+  created: '2025',
+  due: '2026',
+  completed: '2026',
+  userId: 1,
+}
+]
 
 export default function OneTodo() {
-  const todos = {
-    id: '1',
-    task: 'wash cat',
-    urgency: 3,
-    created: '2025',
-    due: '2026',
-    completed: '',
-    userId: '1',
-  }
+
+
 
   // const { data: todos, isPending, error } = useTodos()
   console.log(todos)
 
-  if (isPending) {
-    return (<h2>Loading...</h2>)
-  }
-  if (error) {
-    return (<h2>Error: {error.message}</h2>)
-  }
-  if (!todos) {
-    return (<h2>No todo data found</h2>)
-  }
-
+  // if (isPending) {
+  //   return (<h2>Loading...</h2>)
+  // }
+  // if (error) {
+  //   return (<h2>Error: {error.message}</h2>)
+  // }
+  // if (!todos) {
+  //   return (<h2>No todo data found</h2>)
+  // }
+  //
   return (
-    <h1>One todo component placeholder</h1>
+    <>
+      <h1>All Todos will update to single todo</h1>
+      <ul>
+        {todos.map((todo) => {
+          if (todo.userId === userId) {
+            return (
+              < li key={todo.id} >
+                <h3>{todo.task}</h3>
+                {todo.completed ? <p>complete</p> : null}
+              </li>
+            )
+          }
+          else {
+            return (
+      <p>You&apos;re all caught up</p>
+      <button>Add Todo</button>
+            )
+          }
+        })}
+      </ul >
+    </>
   )
 }
 
