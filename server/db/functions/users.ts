4bd5e901feabc9701e06e55e6cbb95e984ad7e49
Phill Keelan <philliprpkeelan@gmail.com>
2025-04-10 09:53:02 +1200

    db function users

--- server/db/functions/users.ts
+++ server/db/functions/users.ts
@@ -1,6 +1,39 @@
-//This where the functions for
+import db from './../connection.ts'
+// import { User } from '../../../client/models/users.ts'
 
-// getUserByID
+// function convertUsersToSnakeCase(data: user) {
+//   return {
+//     id: data.id,
+//     name: data.name,
+//     avatar_id: data.avatarId,
+//   }
+// }
+
+export const userKeys = [
+  'users.id as id',
+  'users.name as name',
+  'users.avatar_id as avatarId',
+]
+
+// CREATE
+
 // addUser
+
+// READ
+
+export async function getUserById(id: number) {
+  try {
+    const user = await db('users').where('id', id).select(userKeys)
+    return user[0]
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// UPDATE
+
 // updateUser
+
+// DELETE
+
 // deleteUser
