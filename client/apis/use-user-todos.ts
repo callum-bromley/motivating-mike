48bc906d855244c323d06a7fadc24eced4767df8
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-10 15:54:57 +1200

    update todos by userId routes

--- client/apis/use-user-todos.ts
+++ client/apis/use-user-todos.ts
@@ -6,9 +6,9 @@ export default function useUserTodos(userId: number) {
   return useQuery({
     queryKey: ['todos', userId],
     queryFn: async () => {
-      const res = await request.get(`api/v1/todos/${userId}`)
+      const res = await request.get(`api/v1/todos/user/${userId}`)
       if (res.ok) {
-        // console.log('use user todos: ', res.body)
+        console.log('use user todos: ', res.body)
         return res.body as Todo[]
       }
       throw new Error(res.text)
