c6b820cc594aa6483173adec898ddb4174355dcc
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-10 23:09:47 +1200

    get Home page to conditionally render based on login status

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -6,7 +6,6 @@ export default function Nav() {
   const navigate = useNavigate()
 
   const { user, logout, loginWithRedirect } = useAuth0()
-  console.log(user)
 
   const handleSignOut = () => {
     logout()
@@ -18,15 +17,18 @@ export default function Nav() {
 
   return (
     <>
-      <button onClick={() => navigate(`/`)}>Home</button>
-      <button onClick={() => navigate(`/profile`)}>Profile</button>
-      <button onClick={() => navigate(`/todo-list`)}>Todos</button>
       <IfAuthenticated>
+        <button onClick={() => navigate(`/`)}>Home</button>
+        <button onClick={() => navigate(`/profile`)}>Profile</button>
+        <button onClick={() => navigate(`/todo-list`)}>Todos</button>
         <button onClick={handleSignOut}>Sign Out</button>
         {user && <p>Signed in as: {user?.name}</p>}
         <img src={user?.picture} alt='user icon' />
       </IfAuthenticated>
       <IfNotAuthenticated>
+        <button onClick={() => navigate(`/`)}>Home</button>
+        <button onClick={() => navigate(`/login`)}>Profile</button>
+        <button onClick={() => navigate(`/todo-list`)}>Todos</button>
         <button onClick={handleSignIn}>Sign In</button>
       </IfNotAuthenticated>
     </>
--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -4,12 +4,15 @@ import OneHeckle from '../components/OneHeckle'
 import filterTodos from '../components/filteredTodos'
 import HomePageAvatar from '../components/HomePageAvatar'
 import OneTodo from '../components/OneTodo'
+import { IfAuthenticated, IfNotAuthenticated } from '../components/Authenticated'
+// import { useAuth0 } from '@auth0/auth0-react'
 
 const id = 1
 
 export default function Home() {
   const { data: user, isPending, error } = useUserData(id)
   const { data: todos, isPending: todosPending, error: todosError } = useUserTodos(id)
+  // const { getAccessTokenSilently } = useAuth0()
 
   if (isPending || todosPending) {
     return <h2>Loading...</h2>
@@ -25,14 +28,19 @@ export default function Home() {
 
   return (
     <>
-      <h1>Home Page Component placeholder</h1>
-      <OneHeckle
-        userId={user.id}
-        avatarId={user.avatarId}
-        urgency={randomTodo?.urgency}
-      />
-      < HomePageAvatar avatarId={user.avatarId} />
-      <OneTodo todo={randomTodo} />
+      <IfAuthenticated>
+        <h1>Home Page Component placeholder</h1>
+        <OneHeckle
+          userId={user.id}
+          avatarId={user.avatarId}
+          urgency={randomTodo?.urgency}
+        />
+        < HomePageAvatar avatarId={user.avatarId} />
+        <OneTodo todo={randomTodo} />
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <p>Sign in to see your data</p>
+      </IfNotAuthenticated>
     </>
   )
 }
