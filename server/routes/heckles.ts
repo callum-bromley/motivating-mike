64389bb9709969198bcc6cd2b27a2e762aef06af
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-10 15:43:55 +1200

    add heckle hooks,routes,db functions from branch 14

--- server/routes/heckles.ts
+++ server/routes/heckles.ts
@@ -1,5 +1,24 @@
+import express from 'express'
+
+import * as db from '../db/functions/heckles'
+
+const router = express.Router()
+
+// GET /api/v1/heckles
+router.get('/', async (req, res) => {
+  try {
+    const heckles = await db.getHeckles()
+
+    res.json(heckles)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('No heckles loaded')
+  }
+})
 // GET /api/v1/heckles/severity
 //
 // GET /api/v1/heckles/id
 //
 // GET /api/v1/heckles/avatar/id
+
+export default router// GET /api/v1/heckles/severity
