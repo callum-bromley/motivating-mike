ccdc3b05caa19856f85477099dab0eb66bf2aea2
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-10 22:32:15 +1200

    Add SignIn SignOut and usAuth0 to Nav comp

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,13 +1,32 @@
 import { useNavigate } from 'react-router-dom'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+import { useAuth0 } from '@auth0/auth0-react'
 
 export default function Nav() {
   const navigate = useNavigate()
+
+  const { user, logout, loginWithRedirect } = useAuth0()
+  console.log(user)
+
+  const handleSignOut = () => {
+    logout()
+  }
+
+  const handleSignIn = () => {
+    loginWithRedirect()
+  }
 
   return (
     <>
       <button onClick={() => navigate(`/`)}>Home</button>
       <button onClick={() => navigate(`/profile`)}>Profile</button>
       <button onClick={() => navigate(`/todo-list`)}>Todos</button>
+      <IfAuthenticated>
+        <button onClick={handleSignOut}>Sign Out</button>
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <button onClick={handleSignIn}>Sign In</button>
+      </IfNotAuthenticated>
     </>
   )
 }
