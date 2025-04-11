ece4d6de0ca4e4b3c6b7060dd1e88545d3931c73
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-11 21:42:36 +1200

    style Nav bar and add chakra icons

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,10 +1,16 @@
+import { Flex, Icon, Input, useDisclosure, VStack } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 import { useAuth0 } from '@auth0/auth0-react'
+import {
+  Drawer, DrawerBody, DrawerFooter, DrawerHeader, DrawerOverlay, DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
 export default function Nav() {
   const navigate = useNavigate()
-
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const { user, logout, loginWithRedirect } = useAuth0()
 
   const handleSignOut = () => {
@@ -15,22 +21,48 @@ export default function Nav() {
     loginWithRedirect()
   }
 
+
   return (
     <>
-      <IfAuthenticated>
-        <button onClick={() => navigate(`/`)}>Home</button>
-        <button onClick={() => navigate(`/profile`)}>Profile</button>
-        <button onClick={() => navigate(`/todo-list`)}>Todos</button>
-        <button onClick={handleSignOut}>Sign Out</button>
-        {user && <p>Signed in as: {user?.name}</p>}
-        <img src={user?.picture} alt='user icon' />
-      </IfAuthenticated>
-      <IfNotAuthenticated>
-        <button onClick={() => navigate(`/`)}>Home</button>
-        <button onClick={() => navigate(`/login`)}>Profile</button>
-        <button onClick={() => navigate(`/todo-list`)}>Todos</button>
-        <button onClick={handleSignIn}>Sign In</button>
-      </IfNotAuthenticated>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton m={2} />
+          <DrawerHeader></DrawerHeader>
+
+          <DrawerBody>
+            <IfAuthenticated>
+              {user && <p>Username: {user?.name}</p>}
+              <VStack marginRight="auto">
+                <button onClick={() => navigate(`/`)}>Home</button>
+                <button onClick={() => navigate(`/profile`)}>Profile</button>
+                <button onClick={() => navigate(`/todo-list`)}>Todos</button>
+              </VStack>
+            </IfAuthenticated>
+            <IfNotAuthenticated>
+              <button onClick={() => navigate(`/`)}>Home</button>
+              <button onClick={() => navigate(`/login`)}>Profile</button>
+              <button onClick={() => navigate(`/todo-list`)}>Todos</button>
+            </IfNotAuthenticated>
+          </DrawerBody>
+
+          <DrawerFooter>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+      <Flex justify="flex-end" gap={6}>
+        <IfAuthenticated>
+          <button onClick={handleSignOut}>Sign Out</button>
+        </IfAuthenticated>
+        <IfNotAuthenticated>
+          <button onClick={handleSignIn}>Sign In</button>
+        </IfNotAuthenticated>
+        <Icon as={HamburgerIcon} boxSize={6} color='red.500' onClick={onOpen} />
+      </Flex>
     </>
   )
 }
