b3c32bedf39d8330aeeafac2f6add8d1291e19d5
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-13 14:01:41 +1200

    start getting login page to add name

--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -1,10 +1,36 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { IfAuthenticated, IfNotAuthenticated } from "../components/Authenticated";
+import { useAuth0 } from "@auth0/auth0-react";
+import useUserDataAuth from "../apis/use-user-data-auth";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Login() {
+  const { user, loginWithRedirect } = useAuth0()
+  const { data: userData } = useUserDataAuth()
+  const navigate = useNavigate()
+
+  const handleSignIn = () => {
+    loginWithRedirect()
+  }
+
+  useEffect(() => {
+    if (userData) {
+      navigate('/')
+    }
+  }, [userData, navigate])
 
 
+  // if (user) {
+  //   add.mutateAsync({
+  //     newUser: { name, authId: user.sub },
+  //     token,
+  //   })
+  // }
 
+  // If a user signs up/signs in check to see if they have a name if not
+  // render user fields Name* and Pick Avatar
+  // If user does have a name, navigate to home page
 
   return (
     <Box
@@ -17,11 +43,14 @@ export default function Login() {
       alignItems="center"
     >
       <IfNotAuthenticated>
+        <Button onClick={handleSignIn}>Login</Button>
+        <Button onClick={handleSignIn}>Signup</Button>
         <h1>placeholder Login page - user not logged in</h1>
+        <p>Sign in to see your data</p>
       </IfNotAuthenticated>
       <IfAuthenticated>
         <h1>placeholder Login page - user logged in</h1>
       </IfAuthenticated>
-    </Box>
+    </Box >
   )
 }
