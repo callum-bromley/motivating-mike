e7c9d21c5f37b6e12f1910c995d1e14fbfe0e4d1
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-04-13 13:17:31 +1200

    get login component to show a user's logged in status

--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -1,3 +1,27 @@
+import { Box } from "@chakra-ui/react";
+import { IfAuthenticated, IfNotAuthenticated } from "../components/Authenticated";
+
 export default function Login() {
-  return <h1>placeholder Login</h1>
+
+
+
+
+  return (
+    <Box
+      height="100vh"
+      flex="1"
+      flexDir="column"
+      backgroundColor="#B1CFB7"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <IfNotAuthenticated>
+        <h1>placeholder Login page - user not logged in</h1>
+      </IfNotAuthenticated>
+      <IfAuthenticated>
+        <h1>placeholder Login page - user logged in</h1>
+      </IfAuthenticated>
+    </Box>
+  )
 }
